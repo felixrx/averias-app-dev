e1adff6fb799ad227df34f216aaf09866461e15e
felixrx <58267028+felixrx@users.noreply.github.com>
2020-01-02 21:06:59 -0400

    numero de averia

--- db/averias.sql
+++ db/averias.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2019 at 09:18 PM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.23
+-- Generation Time: Jan 03, 2020 at 02:01 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.1.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,6 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `nuevas_averias` (
   `id` int(100) NOT NULL,
+  `numero` text NOT NULL,
   `descripcion` varchar(1000) NOT NULL,
   `cantidad` int(100) NOT NULL,
   `boleto` varchar(100) NOT NULL,
@@ -52,20 +55,8 @@ CREATE TABLE `nuevas_averias` (
 -- Dumping data for table `nuevas_averias`
 --
 
-INSERT INTO `nuevas_averias` (`id`, `descripcion`, `cantidad`, `boleto`, `tipo`, `pueblos`, `zona`, `radio`, `remotos`, `dslams`, `vrads`, `efm`, `gpon`, `cisco3800`, `cisco24g`, `clientes`, `fecha`, `causa`, `comentarios`) VALUES
-(3, 'ssssssss', 333333333, 'ddd', 'ddd', 'ddd', 'ddd', 'ggg', 'nnnn', 'lll', 'ppp', 'mmm', 'rrr', 'sss', 'ppp', 'aaaaaaaaaa', '2019-12-03 00:00:00', 'sssssss', 'fsadfaddddddddddddddddddd\r\ndfgdfg\r\nfghfgh'),
-(5, 'ssssssss', 333333333, 'ddd', 'ddd', 'ddd', 'ddd', 'ggg', 'nnnn', 'lll', 'ppp', 'mmm', 'rrr', 'sss', 'ppp', 'aaaaaaaaaa', '2019-12-02 23:00:00', 'sssssss', 'fsadfaddddddddddddddddddd\r\ndfgdfg\r\nfghfgh'),
-(6, 'ssssssss', 333333333, 'ddd', 'ddd', 'ddd', 'ddd', 'ggg', 'nnnn', 'lll', 'ppp', 'mmm', 'rrr', 'sss', 'ppp', 'aaaaaaaaaa', '2019-12-02 23:00:00', 'sssssss', 'fsadfaddddddddddddddddddd\r\ndfgdfg\r\nfghfgh'),
-(7, 'ffff', 333, 'adfewr', 'c', 'eeeeeeee', 'Metro', 'e', 'dddd', 'rrr', 'ddd', 'j', 'werwqe', 'werwer', '', 'm', '2019-12-10 00:00:00', 'ddd', 'asdasddddddddddddddddddddddddddddddd\njhfgh'),
-(8, 'ffff', 333, 'adfewr', 'c', 'eeeeeeee', 'Metro', 'e', 'dddd', 'rrr', 'ddd', 'j', 'werwqe', 'werwer', '', 'm', '2019-12-10 00:00:00', 'ddd', 'asdasddddddddddddddddddddddddddddddd\njhfgh'),
-(9, 'ffff', 333, 'adfewr', 'c', 'eeeeeeee', 'Metro', 'e', 'dddd', 'rrr', 'ddd', 'j', 'werwqe', 'werwer', '', 'm', '2019-12-10 00:00:00', 'ddd', 'asdasddddddddddddddddddddddddddddddd\njhfgh'),
-(10, 'ffff', 333, 'adfewr', 'c', 'eeeeeeee', 'Metro', 'e', 'dddd', 'rrr', 'ddd', 'j', 'werwqe', 'werwer', '', 'm', '2019-12-10 00:00:00', 'ddd', 'asdasddddddddddddddddddddddddddddddd\njhfgh'),
-(11, 'ffff', 333, 'adfewr', 'c', 'eeeeeeee', 'Metro', 'e', 'dddd', 'rrr', 'ddd', 'j', 'werwqe', 'werwer', '', 'm', '2019-12-10 00:00:00', 'ddd', 'asdasddddddddddddddddddddddddddddddd\njhfgh'),
-(12, 'ffff', 333, 'adfewr', 'c', 'eeeeeeee', 'Metro', 'e', 'dddd', 'rrr', 'ddd', 'j', 'werwqe', 'werwer', '', 'm', '2019-12-10 00:00:00', 'ddd', 'asdasddddddddddddddddddddddddddddddd\njhfgh'),
-(13, 'ffff', 333, 'adfewr', 'c', 'eeeeeeee', 'Metro', 'e', 'dddd', 'rrr', 'ddd', 'j', 'werwqe', 'werwer', '', 'm', '2019-12-10 00:00:00', 'ddd', 'asdasddddddddddddddddddddddddddddddd\njhfgh'),
-(14, 'ffff', 333, 'adfewr', 'c', 'eeeeeeee', 'Metro', 'e', 'dddd', 'rrr', 'ddd', 'j', 'werwqe', 'werwer', '', 'm', '2019-12-10 00:00:00', 'ddd', 'asdasddddddddddddddddddddddddddddddd\njhfgh'),
-(15, 'ffff', 333, 'adfewr', 'c', 'eeeeeeee', 'Metro', 'e', 'dddd', 'rrr', 'ddd', 'j', 'werwqe', 'werwer', '', 'm', '2019-12-10 00:00:00', 'ddd', 'asdasddddddddddddddddddddddddddddddd\njhfgh'),
-(16, 'ffff', 333, 'adfewr', 'c', 'eeeeeeee', 'Metro', 'e', 'dddd', 'rrr', 'ddd', 'j', 'werwqe', 'werwer', '', 'm', '2019-12-10 00:00:00', 'ddd', 'asdasddddddddddddddddddddddddddddddd\njhfgh');
+INSERT INTO `nuevas_averias` (`id`, `numero`, `descripcion`, `cantidad`, `boleto`, `tipo`, `pueblos`, `zona`, `radio`, `remotos`, `dslams`, `vrads`, `efm`, `gpon`, `cisco3800`, `cisco24g`, `clientes`, `fecha`, `causa`, `comentarios`) VALUES
+(17, '1578013216709', 'll', 88, 'tt', 'tt', 'rr', 'Metro', 'uuu', 'ttt', 'rr', 'dd', 'ff', 'fff', 'fff', 'hh', 'ddd', '2020-01-03 00:00:00', 'uuu', 'jjjjjj\n');
 
 --
 -- Indexes for dumped tables
@@ -85,7 +76,9 @@ ALTER TABLE `nuevas_averias`
 -- AUTO_INCREMENT for table `nuevas_averias`
 --
 ALTER TABLE `nuevas_averias`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
